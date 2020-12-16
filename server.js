da2f115b1a03fa739de25fe5ad6fdf512f9e24e3
Brian Clayton <finalb@projectfinalboss.com>
2020-12-15 23:46:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,12 @@
 const express = require("express");
 const helmet = require('helmet');
-const session = require('express-session');
 
-const UserRouter = require('./users/user-router');
-const { restart } = require("nodemon");
 
 const server = express();
 
-const sessionConfig = {
-    name: 'secretfamrecipesess',
-    secret: 'crack this, and I"ll crack you.',
-    cookie: 
-    {
-     maxAge:    1000 * 30,
-     secure: false,
-     httpOnly: true
-    },
-    resave: false,
-    saveUninitialize: false
-
-}
 
 server.use(helmet());
 server.use(express.json());
-server.use(session(sessionConfig));
 
 
 
