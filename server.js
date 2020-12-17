7d050c40cbc0e9dc9c6d08e8eac2e3c50f6be4d6
Brian Clayton <finalb@projectfinalboss.com>
2020-12-16 22:19:33 -0500

    update

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const sessionConfig = {
     secret: 'asifIdtellyouthesecretsauce',
     cookie: {
       maxAge: 1000 * 60 * 60,
-      secure: false, 
+      secure: true, 
       httpOnly: true
     },
     resave: false,
